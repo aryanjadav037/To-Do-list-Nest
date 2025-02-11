5bb05d4d4d3ff5176d51b48c2bc7e0ac8fc1a757
aryan <aryanjadav081103@gmail.com>
2025-02-11 12:08:39 +0530

    Add Swagger support for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -7,6 +8,15 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Todos API')
+    .setDescription('The Todo API description')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   try {
     await app.listen(3000);
     console.log('Server is running on http://localhost:3000');
